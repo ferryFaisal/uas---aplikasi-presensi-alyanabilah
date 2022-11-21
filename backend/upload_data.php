b034e7d1f680f5a0d3144dd91cf38588a9b039f4
alyanabilah <116285415+alyanabilah@users.noreply.github.com>
2022-11-21 18:30:07 +0700

    menambahkan file backend

---
+++ backend/upload_data.php
@@ -0,0 +1,25 @@
+<?php
+require 'connect_db.php';
+
+// session_start();
+$nim = $_POST['nim'];
+$nama = $_POST['nama'];
+$class = $_POST['kelas'];
+
+$sql = "INSERT INTO mahasiswa (nim, nama, kelas)
+    VALUES ('$nim',
+    '$nama',
+    '$class'
+    )";
+
+if (mysqli_query($conn, $sql)) {
+    echo "data berhasil dimasukkan ke database";
+
+    header('Location: tables-product.php');
+    ob_end_flush();
+
+} else {
+    echo "gagal memasukkan data: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
